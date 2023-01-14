88d19bce33b0f60370a8338af29540d6d5a60aba
Jacoby Lockman <jlockman@g.hmc.edu>
2023-01-13 20:33:05 -0800

    A user can now edit their blogs.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,8 +6,16 @@ class DashboardsController < ApplicationController
   end
 
   def edit
-    @blog = Blog.find_by(params[:id])
-  
+    session[:blog_id] = params[:id]
+    @test = session[:blog_id]
+    @blog = Blog.find_by(id: params[:id])
+    @user = User.find_by(id: session[:user_id])
+    if @blog.user_id != @user.id
+      flash[:alert] = "You are not the author of that blog"
+      redirect_to dashboards_path
+    end
+
+      
   end
 
   def post
@@ -26,6 +34,9 @@ class DashboardsController < ApplicationController
     @blog = Blog.new(dash_params)
     @user = User.find_by(id: session[:user_id])
     @blog.user = @user
+
+    puts " ++++++++++++++++++++++++ #{session[:blog_id]}"
+
     if @blog.save     
       redirect_to dashboard_path(@blog)
     else
@@ -36,15 +47,19 @@ class DashboardsController < ApplicationController
   end  
 
   def update
-    @blog = Blog.find_by(params[:id])
-
+    @id = params[:id]
+    @user = User.find_by(id: session[:user_id])
+    @blog = Blog.find_by(id: session[:blog_id])
+    puts "****************** #{session[:blog_id]}"
     if @blog.update(params.require(:blog).permit(:title, :blog))
-      flash[:notice] = "blog was updated successfully"
+      flash[:alert] = "Blog updated succesfully"
       redirect_to dashboards_path
-      else
-          render :index, status: unprocessable_entity
-      end
+    else
+      flash[:alert] = "Blog not updated succesfully"
+      render :edit, status: :unprocessable_entity
+
     end
+  end
 
   private
     def dash_params
